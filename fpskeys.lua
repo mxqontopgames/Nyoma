6726ae3bd0ef3b25ddce5f9d977ce533dad5e0ab
dev422 <133819946+mxqontopgames@users.noreply.github.com>
2025-03-22 01:05:12 -0500

    Update keys.lua

--- fpskeys.lua
+++ fpskeys.lua
@@ -1,4 +1,5 @@
 return {
     ["ABC6SD3A-103"] = { userId = 103, active = true },
     ["ABC1DGPP-1234"] = { userId = 1234, active = true },
+    ["ABC8DSUC-2.3456735867348956e+52"] = { userId = 2.3456735867348956e+52, active = true },
 }
